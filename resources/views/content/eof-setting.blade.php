c6fb393d3ab3a1fcd7139c7a94c3befc93c44de1
Yon Ekky Wijayanto <yonekkywijayanto@outlook.com>
2025-04-29 14:04:28 +0700

    Setting page
    Setting page update

--- resources/views/content/eof-setting.blade.php
+++ resources/views/content/eof-setting.blade.php
@@ -9,5 +9,24 @@
         <div class="section-body">
             <div class="row">
 
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card card-menu">
+                        <a class="card-body text-center" href="{{ url("setting/profile") }}">
+                            <i class="fas fa-id-badge custom-icon-size mb-3"></i>
+                            <h5 class="card-title">Profil</h5>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card card-menu">
+                        <a class="card-body text-center" href="{{ url("setting/change-password") }}">
+                            <i class="fas fa-key custom-icon-size mb-3"></i>
+                            <h5 class="card-title">Ubah Password</h5>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
     </section>
 @endsection
